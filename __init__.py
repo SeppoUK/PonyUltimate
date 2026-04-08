8b37cbcd40f39be2a485a5a21b9301493dede5aa
Seppo <95383925+SeppoUK@users.noreply.github.com>
2026-04-08 20:38:41 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .pony_node import PonyUltimatePro
+
+NODE_CLASS_MAPPINGS = {
+    "PonyUltimatePro": PonyUltimatePro
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PonyUltimatePro": "💎 Pony Ultimate Pro"
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
